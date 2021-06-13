378c6d14d016aeffb2001ae008ead01c53392efc
Pedro Alves <pedro22carro@gmail.com>
2021-06-13 19:47:16 -0300

    relogio

--- relogio/src/App.js
+++ relogio/src/App.js
@@ -1,54 +1,63 @@
-// Exemplo de relógio para ilustrar o ciclo de vida de um componente
-
 import React from 'react';
 import './App.css';
 
-// Define uma função DataFormatada que retorna o subtítulo com o valor da data formatada
-function DataFormatada(props){
-  return <h2>Horário Atual: {props.date.toLocaleTimeString()}</h2>
-};
+// Função que formata a data. Retorna apenas a hora
+function FormattedDate(props) {
+  return <h2>Horário atual: {props.date.toLocaleTimeString()}</h2>
+}
 
-// Define a class Clock que será chamada na renderização dentro da App
-class Clock extends React.Component{
-  constructor(props){
+// Define a classe Clock que será renderizada dentro do App
+class Clock extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      // Define o estado date pegando a data atual
-      date : new Date()
+      // Define o estado date pegado a data atual
+      date: new Date()
     };
   }
 
-  // Ciclo de vida que ocorre quando Clock é inserida no DOM
-  // Através do setInterval, o relógio é criado (com um timerID atrelado)
-  // Chama a função thick a cada 1000ms (1s)
-  componentDidMount(){
-    this.timerID = setInterval(() => {
-      this.thick()
+  // Clico de vida que ocorre quando o Clock é inserido na DOM
+  // Através do setInterval o relógio é atualizada a cada segundo
+  componentDidMount() {
+    this.timerId = setInterval(() => {
+      this.tick()
     }, 1000);
-
-    // Exibe no console o ID de cada relógio
-    console.log("Eu sou o relógio " + this.timerID);
   }
 
-  // Ciclo de vida que ocorre quando o componento é removido do DOM
-  // Quando isso ocorre, a função clearInterval limpa o relógio criado pelo setInterval
-  componentWillUnmount(){
-    clearInterval(this.timerID);
+  // Cliclo de vida que ocorre quando o Clock é removido da DOM
+  // A função clearInterval limpa o relógio criado pelo setInterval
+  componentWillUnmount() {
+    clearInterval(this.timerId);
   }
 
-  // Define no state date a data atual a cada vez que é chamada
-  thick(){
+  // Define no state date a data atual toda vez que é chamda
+  tick() {
     this.setState({
       date: new Date()
     });
   }
 
-  // Renderiza na tela o título e a função DataFormada, passando date com o valor atual do state
-  render(){
-    return(
+  // Pausa o relógio
+  Pause() {
+    clearInterval(this.timerId);
+  }
+
+  // Atualiza o relógio para hora atual
+  Continue() {
+    this.timerId = setInterval(() => {
+      this.tick()
+    }, 1000);
+  }
+  
+  // Renderiza o h1 e a função FormattedDate com o valor atual do state
+  render() {
+    return (
       <div>
         <h1>Relógio</h1>
-        <DataFormatada date={this.state.date} />
+        {/* Chama a função FormattedDate e passa a date como prop */}
+        <FormattedDate date = {this.state.date} />
+        <button className="btn-Pause" onClick={() => this.Pause()}>Pause</button>
+        <button className="btn-Continue" onClick={() => this.Continue()}>Continue</button>
       </div>
     )
   }
@@ -59,7 +68,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Faz a chamada de dois relógios para mostrar a independência destes */}
+        {/* Chama dois Clock's para mostrar suas independências */}
         <Clock />
         <Clock />
       </header>
@@ -67,5 +76,5 @@ function App() {
   );
 }
 
-// Declara que a função App pode ser usada fora do escopo dela mesma
+// Declara que a função App pode ser usada fora do seu escopo
 export default App;
