e3fd129e4a39039fe905f13c6c188ffad126d744
Pedro Alves <pedro22carro@gmail.com>
2021-06-13 17:29:13 -0300

    desafio processo seletivo

--- desafio_processo_seletivo/src/App.jsx
+++ desafio_processo_seletivo/src/App.jsx
@@ -1,35 +1,71 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-class Pesquisa extends Component{
+export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      usuario:''
-    }
+      repositories: [],
+      username: ''
+    };
+  };
+
+  reposQuery = (e) => {
+    e.preventDefault();
+
+    fetch(`http://api.github.com/users/${this.state.username}/repos?sort=created&per_page=10`)
+    .then(response => response.json())
+    .then(data => this.setState({repositories: data}))
+    .catch(error => console.log(error))
   }
-}
 
-FazerPesquisa = (usuario) => {
-  fetch('https://api.github.com/'+usuario+'/repos')
-
-  .then(resposta => resposta.json())
-
-  .catch( erro => console.log(erro) )
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <form >
-          <input type="text" placeholder="Nome Usuario"/>
-          <button>Pesquisar</button>
-        </form>
+  render() {
+    return(
+      <div>
+        <header>
+          <h1>Github Repositories</h1>
+        </header>
         
-      </header>
-    </div>
-  );
-}
+        <section id="input">
+          <form onSubmit={this.reposQuery}>
+            <input 
+              type='text'
+              value={this.state.username}
+              onChange={e => this.setState({username: e.target.value})}
+            />
+            <button type='submit'>Buscar</button>
+          </form>
+        </section>
 
-export default App;
+        <section>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Data</th>
+                <th>Tamanho</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.repositories.map(r => {
+                  return (
+                    <tr key={r.id}>
+                      <td>{r.id}</td>
+                      <td>{r.name}</td>
+                      <td>{r.description}</td>
+                      <td>{r.created_at}</td>
+                      <td>{r.size}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </section>
+      </div>
+    )
+  }
+};
